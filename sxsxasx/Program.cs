44d36447e39a11dcfffaec6b00620af21c9391cd
ffk1x <sxaxq7@gmail.com>
2022-11-07 21:11:23 +0300

    Добавьте файлы проекта.

---
+++ sxsxasx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sxsxasx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            RenderUkraineFlag();
+            Console.ReadKey();
+        }
+
+        static void RenderUkraineFlag()
+        {
+            for (int i = 0; i <= 10000; i++)
+            {
+                if (i < 5000)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Write("=");
+                    Console.ResetColor();
+                }
+                else if (i >= 5000)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Write("=");
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
